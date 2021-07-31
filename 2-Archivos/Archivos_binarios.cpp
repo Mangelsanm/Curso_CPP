cf407d50b21289eb9fb0570f9df55d86143bf54f
Miguel Sanchez-Mares (uidg9784) <miguel.sanchez-mares@continental-corporation.com>
2021-07-30 22:00:17 -0500

    Programa para crear y leer archivos binarios

--- 2-Archivos/Archivos_binarios.cpp
+++ 2-Archivos/Archivos_binarios.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 #pragma pack(push, 1)
@@ -10,6 +11,38 @@ struct Person {
 #pragma pack(pop)
 
 int main() {
-    cout << sizeof(Person) << endl;
+    Person person = {"Miguel", 34, 85};
+
+    ofstream outFile;
+    char const *fileName = "test.bin";
+
+    outFile.open(fileName);
+
+    if(outFile.is_open()) {
+        outFile.write(reinterpret_cast<char *>(&person), sizeof(Person));
+        outFile.close();
+    }
+    else {
+        cout << "El archivo no fue creado" << endl;
+    }
+
+    //Leer el archivo binario
+    Person otherPerson = {};
+    
+    ifstream inFile;
+    // const char *inputFile = "test.bin";
+
+    inFile.open(fileName, ios::binary);
+    
+    if(inFile.is_open()) {
+        inFile.read(reinterpret_cast<char *>(&otherPerson), sizeof(Person));
+        inFile.close();
+    }
+    else {
+        cout << "El archivo no puede ser leido" << endl;
+    }
+
+    cout << otherPerson.name << " " << otherPerson.age << " " << otherPerson.weight << endl;
+
     return 0;
 }
